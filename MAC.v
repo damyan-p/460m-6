bf8f912d2010529f6732a26adb4554e434b3f657
dpavlovic <dpavlovic@utexas.edu>
2022-04-18 10:56:35 -0500

    branch control for today

--- MAC.v
+++ MAC.v
@@ -3,17 +3,198 @@ module MAC(
 input clk,
 input [7:0] ain,
 input [7:0] bin,
-output reg [8:0] out,//matrix outputs
-output reg [7:0] aout, bout
+output reg [7:0] out,//matrix outputs
+output [7:0] apass, bpass
     );
- 
-wire [7:0] addresult, mulresult;
-add a0(.clk(clk),.op1(ain),.op2(bin),.res(addresult));
-mul m0(.clk(clk),.op1(ain),.op2(addresut),.res(out));
     
-always @(posedge clk)begin
-out = out + (ain*bin);
-aout = ain;
-bout = bin;
+reg [7:0] temp;
+reg [7:0] aprev;
+reg [7:0] bprev;
+//multiply variables
+reg [4:0] mF1,mF2; 
+reg [2:0] mE1,mE2;
+reg [2:0] mebuf;
+reg [9:0] mfbuf;
+wire msbuf = 0;
+//add variables
+reg [12:0] aF1,aF2; 
+reg [2:0] aE1,aE2;
+reg aS1,aS2;
+reg [13:0] afracsum;//added extra bit
+reg [2:0] aebuf;
+reg asbuf = 0;
+reg aovf;
+initial begin
+temp <= 0;
+out <= 0;
+aprev <= 0;
+bprev <= 0;
+mF1 <= 0;
+mF2 <= 0;
+aF1 <= 0;
+aF2 <= 0;
 end
+assign apass = aprev;
+assign bpass = bprev;
+
+
+//temp = ain * bin;taken care of by multest
+//out = out + temp;addtest
+always @(posedge clk) begin
+aprev = ain;
+bprev = bin;
+//------------------multiply--------------------//
+mF1 = {1'b1,ain[3:0]};
+mF2 = {1'b1,bin[3:0]};
+mE1 = ain[6:4] - 3;
+mE2 = bin[6:4] - 3;
+mebuf = mE1 + mE2 + 3;
+mfbuf = mF1 * mF2;
+if(ain == 0 || bin == 0) begin
+temp = 0;
+end else begin
+  if(mfbuf == 0)begin
+ temp = {(ain[7]^bin[7]),mebuf,4'b0000};
+ end 
+ else begin
+ if(mfbuf[9])begin
+ mfbuf = mfbuf >> 1;
+ mebuf = mebuf + 1;
+ end
+ if(mfbuf[9]==0 && mfbuf[8]==0)begin
+ mfbuf = mfbuf << 1;
+ mebuf = mebuf - 1;
+ end
+ if(mfbuf[9]==0 && mfbuf[8]==0)begin
+ mfbuf = mfbuf << 1;
+ mebuf = mebuf - 1;
+ end
+ if(mfbuf[9]==0 && mfbuf[8]==0)begin
+ mfbuf = mfbuf << 1;
+ mebuf = mebuf - 1;
+ end
+ if(mfbuf[9]==0 && mfbuf[8]==0)begin
+ mfbuf = mfbuf << 1;
+ mebuf = mebuf - 1;
+ end
+ if(mfbuf[9]==0 && mfbuf[8]==0)begin
+ mfbuf = mfbuf << 1;
+ mebuf = mebuf - 1;
+ end
+ if(mfbuf[9]==0 && mfbuf[8]==0)begin
+ mfbuf = mfbuf << 1;
+ mebuf = mebuf - 1;
+ end
+ if(mfbuf[9]==0 && mfbuf[8]==0)begin
+ mfbuf = mfbuf << 1;
+ mebuf = mebuf - 1;
+ end
+ if(mfbuf[9]==0 && mfbuf[8]==0)begin
+ mfbuf = mfbuf << 1;
+ mebuf = mebuf - 1;
+ end
+ if(mfbuf[9]==0 && mfbuf[8]==0)begin
+ mfbuf = mfbuf << 1;
+ mebuf = mebuf - 1;
+ end
+ else begin
+  temp = {(ain[7]^bin[7]),mebuf,mfbuf[7:4]};
+ end
+end
+end
+//---------------adding-----------------------------//
+//op1 out
+//op2 temp
+    aF1[12:7] = {out[7],1'b1,out[3:0]};
+    aF2[12:7] = {temp[7],1'b1,temp[3:0]};
+    aE1 = out[6:4];
+    aE2 = temp[6:4];
+    aS1 = out[7];
+    aS2 = temp[7];
+    if(out == 0) begin
+    out = temp;
+    end else if(temp == 0) begin
+    out = out;
+    end else begin
+    if(aE1 == aE2)begin
+    aebuf = aE1;
+    asbuf = aS1;
+    end
+    else if (aE1 < aE2)begin
+     asbuf = aS2;
+     aF1[11:0] = aF1[11:0] >> (aE2 - aE1);
+     aebuf = aE1 + (aE2 - aE1);
+     end
+    else if(aE1 > aE2)begin
+    asbuf = aS1;
+     aF2[11:0] = aF2[11:0] >> (aE1 - aE2);
+     aebuf = aE2 + (aE1 - aE2);
+    end
+
+
+    afracsum = aF1 + aF2;
+    aovf = (~afracsum[13]&&aF1[12]&&aF2[12])||(afracsum[13]&&(~aF1[12])&&(~aF2[12]));
+    if(aovf)begin
+    afracsum = afracsum >> 1;
+    aebuf = aebuf + 1;
+    end
+    
+    if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+          if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+             if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+           if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+                if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+                 if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+        if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+           if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+       if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+            if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+           if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+        if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+           if(afracsum[11] != 1 && (afracsum != 0)) begin//not normalized
+    afracsum[11:0] <= afracsum[11:0] << 1;
+    aebuf <= aebuf - 1;
+    end //else begin
+    out <= {asbuf, aebuf, afracsum[10:7]};
+
+end
+end
+
+
 endmodule
--- top.v
+++ top.v
@@ -3,15 +3,113 @@ module top(
 input clk,
 input [7:0] a00, a01, a02, a10, a11, a12, a20, a21, a22,
 input [7:0] b00, b01, b02, b10, b11, b12, b20, b21, b22,
-output reg [7:0] c1,c2,c3,c4,c5,c6,c7,c8//matrix to output
+output [7:0] c1,c2,c3,c4,c5,c6,c7,c8,c9//matrix to output
     );
-    
-//  c1  c2  c3
-//  c4  c5  c6
-//  c7  c8  c9    
 
-wire [7:0] c12,c13,c22,c23,c33,c42,c43,c52,c53,c63,c72,c82;
+    reg [2:0] state;
+    reg [2:0] next_state; 
 
-MAC m1(.clk(clk), .ain(a00), .bin(b00), .out(c1), .aout(c12), .bout(bc13));
+    reg [7:0] Rin [2:0];
+    reg [7:0] Cin [2:0];
+    wire [7:0] c12,c23,c45,c56,c78,c89,c14,c25,c36,c47,c58,c69;
+    wire [7:0] c3x,c6x,c9x,cx7,cx8,cx9;
 
+    initial begin
+    Cin[2] <= 0;
+    Cin[1] <= 0;
+    Cin[0] <= 0;
+    Rin[2] <= 0;
+    Rin[1] <= 0;
+    Rin[0] <= 0;
+    state <= 0;
+    next_state <= 0;
+    end
+
+//          [0] [1] [2]  Cin    
+//  Rin
+// [0]->    c1  c2  c3
+// [1]->    c4  c5  c6
+// [2]->    c7  c8  c9   
+
+    MAC m1(.ain(Rin[0]), .bin(Cin[0]), .out(c1),.apass(c12),.bpass(c14));
+    MAC m2(.ain(c12), .bin(Cin[1]), .out(c2),.apass(c23),.bpass(c25));
+    MAC m3(.ain(c23), .bin(Cin[2]), .out(c3),.apass(c3x),.bpass(c36));
+    MAC m4(.ain(Rin[1]), .bin(c14), .out(c4),.apass(c45),.bpass(c46));
+    MAC m5(.ain(c45), .bin(c25), .out(c5),.apass(c56),.bpass(c58));
+    MAC m6(.ain(c56), .bin(c36), .out(c6),.apass(c6x),.bpass(c69));
+    MAC m7(.ain(Rin[2]), .bin(c47), .out(c7),.apass(c78),.bpass(cx7));
+    MAC m8(.ain(c78), .bin(c58), .out(c8),.apass(c89),.bpass(cx8));
+    MAC m9(.ain(c89), .bin(c69), .out(c9),.apass(c9x),.bpass(cx9));
+
+    always @(state) begin
+    case(state)
+    1: begin
+    Rin[0] = a01;
+    Rin[1] = a10;
+    Rin[2] = 0;
+    Cin[0] = b10;
+    Cin[1] = b01;
+    Cin[2] = 0;
+    next_state <= 2;
+    end
+    0: begin
+    Rin[0] = a00;
+    Rin[1] = 0;
+    Rin[2] = 0;
+    Cin[0] = b00;
+    Cin[1] = 0;
+    Cin[2] = 0;
+    next_state <= 1;
+    end
+    2: begin
+    Rin[0] = a02;
+    Rin[1] = a11;
+    Rin[2] = a20;
+    Cin[0] = b20;
+    Cin[1] = b11;
+    Cin[2] = b02;
+    next_state <= 3;
+    end
+    3: begin
+    Rin[0] = 0;
+    Rin[1] = a12;
+    Rin[2] = a21;
+    Cin[0] = 0;
+    Cin[1] = b21;
+    Cin[2] = b12;
+    next_state <= 4;
+    end
+    4: begin
+    Rin[0] = 0;
+    Rin[1] = 0;
+    Rin[2] = a22;
+    Cin[0] = 0;
+    Cin[1] = 0;
+    Cin[2] = b22;
+    next_state <= 5;
+    end
+    5: begin
+    Rin[0] = 0;
+    Rin[1] = 0;
+    Rin[2] = 0;
+    Cin[0] = 0;
+    Cin[1] = 0;
+    Cin[2] = 0;
+    next_state <= 6;
+    end
+    6:begin
+    Rin[0] = 0;
+    Rin[1] = 0;
+    Rin[2] = 0;
+    Cin[0] = 0;
+    Cin[1] = 0;
+    Cin[2] = 0;
+    next_state <= 6;
+    end
+    endcase
+    end
+
+    always @(posedge clk) begin
+    state <= next_state;
+    end
 endmodule
